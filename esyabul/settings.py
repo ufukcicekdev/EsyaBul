a8e34342e795858c4bdab5a58cda058d1fb2d2fb
EAGLEEARTH <ufukcicek199@gmail.com>
2024-02-07 21:04:15 +0300

    push

--- esyabul/settings.py
+++ esyabul/settings.py
@@ -413,7 +413,6 @@ CKEDITOR_5_CONFIGS = {
     },
 }
 
-CKEDITOR_BASEPATH = os.path.join(BASE_DIR, 'django_ckeditor_5')
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
--- esyabul/urls.py
+++ esyabul/urls.py
@@ -18,8 +18,11 @@ urlpatterns = [
 
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "main.views.custom_404_page"
 handler500 = "main.views.custom_500_page"
