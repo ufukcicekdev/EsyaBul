c994124dca6925724f6414f2c9109c84e12bbbaa
EAGLEEARTH <ufukcicek199@gmail.com>
2024-02-08 17:06:29 +0300

    push

--- esyabul/settings.py
+++ esyabul/settings.py
@@ -155,8 +155,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
-
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
