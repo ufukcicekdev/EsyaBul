fde4acfde68edcee8d88d6fd31af754ddb1173f5
EAGLEEARTH <ufukcicek199@gmail.com>
2024-02-08 15:08:50 +0300

    push

--- esyabul/settings.py
+++ esyabul/settings.py
@@ -152,13 +152,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 
 
 # Default primary key field type
