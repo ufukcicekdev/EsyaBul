15aa6e34bb8f24049e26d5716bd26b38fa666a4c
EAGLEEARTH <ufukcicek199@gmail.com>
2024-03-29 23:08:06 +0300

    push

--- esyabul/urls.py
+++ esyabul/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -17,9 +15,8 @@ urlpatterns = [
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "main.views.custom_404_page"
 handler500 = "main.views.custom_500_page"
