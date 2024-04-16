0a89a94f5af274b992bde6ed46fee353380b524f
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-17 00:55:20 +0300

    push

--- Jobs/tasks.py
+++ Jobs/tasks.py
@@ -6,9 +6,9 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(send_email_notifications, 'interval', minutes=1)
-    #scheduler.add_job(check_wishlist, 'cron', day_of_week='mon', hour=9)
+    scheduler.add_job(check_wishlist, 'cron', day_of_week='mon', hour=9)
 
-    scheduler.add_job(check_wishlist, 'interval', minutes=1)
+    #scheduler.add_job(check_wishlist, 'interval', minutes=1)
 
 
 
