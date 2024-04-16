8972e2de1f51efbbf4978c3dd7f607bbb518820e
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-16 22:00:17 +0300

    push

--- esyabul/settings.py
+++ esyabul/settings.py
@@ -29,10 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 
 
-ALLOWED_HOSTS = ["esyala.com"]
 
 CSRF_TRUSTED_ORIGINS = ["https://*."]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
