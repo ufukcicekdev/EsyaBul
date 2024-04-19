08e59223a61c9e2b2fc2f34855817c9e4057d63e
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-19 21:46:40 +0300

    push

--- esyabul/settings.py
+++ esyabul/settings.py
@@ -29,15 +29,15 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
 
 
-# ALLOWED_HOSTS = ["esyala.com"]
-# CSRF_TRUSTED_ORIGINS = ["https://*."]
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+ALLOWED_HOSTS = ["esyala.com"]
+CSRF_TRUSTED_ORIGINS = ["https://*."]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 INSTALLED_APPS = [
