35d16c5caa0b3e6fb9c774d1cc24b36c8a135c2b
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-21 13:44:58 +0300

    randomFilename func added

--- esyabul/storage_backends.py
+++ esyabul/storage_backends.py
@@ -1,4 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+import os
+import uuid
 
 class CustomS3Boto3Storage(S3Boto3Storage):
-    pass
+    def get_available_name(self, name, max_length=None):
+        base_name, ext = os.path.splitext(name)
+        random_hex = uuid.uuid4().hex
+        new_name = f"{base_name}_{random_hex}{ext}"
+        folder_name = self.location
+        final_name = os.path.join(folder_name, new_name)
+        return super().get_available_name(final_name, max_length)
