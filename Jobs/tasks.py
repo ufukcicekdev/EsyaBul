404a4087c6d38926ef499076fafb5bbc2334e7af
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-24 23:52:15 +0300

    expired date products mail added

--- Jobs/tasks.py
+++ Jobs/tasks.py
@@ -7,7 +7,8 @@ def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(send_email_notifications, 'interval', minutes=1)
     scheduler.add_job(check_wishlist, 'cron', day_of_week='mon', hour=9)
-    scheduler.add_job(notify_users_about_expiring_orders, 'interval', minutes=1)
+    scheduler.add_job(notify_users_about_expiring_orders, 'interval', days=2, hours=9)
+
 
     #scheduler.add_job(check_wishlist, 'interval', minutes=1)
 
