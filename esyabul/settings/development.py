660641b9d97afaad235cdc718b267d6deb9955da
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-25 21:12:24 +0300

    push

--- esyabul/settings/development.py
+++ esyabul/settings/development.py
@@ -1,27 +1,19 @@
 from esyabul.settings.base import * 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
-ALLOWED_HOSTS = ["esyala.com"]
-CSRF_TRUSTED_ORIGINS = ["https://*."]
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-
-
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('PROD_DATABASE_ENGINE'),
-        'NAME': os.getenv('PROD_DATABASE_NAME'),
-        'USER': os.getenv('PROD_DATABASE_USER'),
-        'PASSWORD': os.getenv('PROD_DATABASE_PASSWORD'),
-        'HOST': os.getenv('PROD_DATABASE_HOST'),
-        'PORT': os.getenv('PROD_DATABASE_PORT'),
+        'ENGINE': os.getenv('DEV_DATABASE_ENGINE'),
+        'NAME': os.getenv('DEV_DATABASE_NAME'),
+        'USER': os.getenv('DEV_DATABASE_USER'),
+        'PASSWORD': os.getenv('DEV_DATABASE_PASSWORD'),
+        'HOST': os.getenv('DEV_DATABASE_HOST'),
+        'PORT': os.getenv('DEV_DATABASE_PORT'),
         'OPTIONS': {
             'sslmode': 'require',  # SSL gereklilik durumu
         },
@@ -29,12 +21,12 @@ DATABASES = {
 }
 
 
-AWS_ACCESS_KEY_ID = os.getenv('PROD_AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('PROD_AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('PROD_AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = os.getenv('PROD_AWS_S3_CUSTOM_DOMAIN')
-AWS_DEFAULT_ACL = os.getenv('PROD_AWS_DEFAULT_ACL')
-AWS_S3_REGION_NAME = os.getenv("PROD_AWS_S3_REGION_NAME")
-AWS_S3_ENDPOINT_URL = os.getenv("PROD_AWS_S3_ENDPOINT_URL")
-AWS_LOCATION = os.getenv("PROD_AWS_LOCATION")
+AWS_ACCESS_KEY_ID = os.getenv('DEV_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('DEV_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('DEV_AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = os.getenv('DEV_AWS_S3_CUSTOM_DOMAIN')
+AWS_DEFAULT_ACL = os.getenv('DEV_AWS_DEFAULT_ACL')
+AWS_S3_REGION_NAME = os.getenv("DEV_AWS_S3_REGION_NAME")
+AWS_S3_ENDPOINT_URL = os.getenv("DEV_AWS_S3_ENDPOINT_URL")
+AWS_LOCATION = os.getenv("DEV_AWS_LOCATION")
 DEFAULT_FILE_STORAGE = 'esyabul.storage_backends.CustomS3Boto3Storage'
