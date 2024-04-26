cab8d8bd872b5264b1e8389c787fa2bad5ef1a7c
EAGLEEARTH <ufukcicek199@gmail.com>
2024-04-26 16:06:11 +0300

    sitemap added

--- main/sitemap.py
+++ main/sitemap.py
@@ -12,9 +12,24 @@ enddate = startdate + relativedelta(years=10)
 
 
 
+class StaticViewSitemap(Sitemap):
+    def items(self):
+        return ['home','contact', 'about', 'faqs',]  
+
+    def location(self, item):
+        if item == 'home':
+            return ''
+        elif item == 'about':
+            return '/about/'
+        elif item == 'contact':
+            return '/contact/'
+        elif item == 'faqs':
+            return '/faqs/'
+        else:
+            return reverse(item)
 
 
-    
+
 class ProductLinkSiteMap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
@@ -27,3 +42,15 @@ class ProductLinkSiteMap(Sitemap):
         return f"/product/{obj.slug}"
     
 
+
+class CategoryLinkSiteMap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return Category.objects.filter(is_active=True)
+
+    def location(self,obj):
+        return f"/category/{obj.get_full_path_slug()}"
+    
