5bde1d0d704ffd7afde80c7b4ad6ecd9c51cf342
EAGLEEARTH <ufukcicek199@gmail.com>
2024-05-06 20:34:51 +0300

    push

--- main/admin.py
+++ main/admin.py
@@ -37,9 +37,8 @@ class HomeSubBannerAdmin(admin.ModelAdmin):
     readonly_fields = ('image_preview',)
 
     def image_preview(self, obj):
-        return obj.image.url if obj.image else None
+        return obj.image_preview()
     image_preview.short_description = 'Image Preview'
-    image_preview.allow_tags = True
 
 admin.site.register(HomeSubBanner, HomeSubBannerAdmin)
 
