e5e41e57fbfb400d232c391d96fcf78cd0f3410d
EAGLEEARTH <ufukcicek199@gmail.com>
2024-09-26 13:12:13 +0300

    push

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import *
 app_name = "blog"
 
 urlpatterns = [
-   path('blog', bloghome, name='blog_home'),
+   path('blog/', bloghome, name='blog_home'),
    path('blog/<slug:slug>/', blog_detail, name='blog_detail'),
    path('blogcategory/<slug:slug>/', category_detail, name='category_detail_blog'),
 ]
